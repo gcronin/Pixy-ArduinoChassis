445f01cfb06d6e97f2e3a4a63615e13ac5c12857
gcronin <gcronin@seattleacademy.org>
2017-07-07 09:59:03 -0700

    Added Potentiometer Control of BaseSpeed and Proportionality Constant

--- FollowYellowGreenSquares/FollowYellowGreenSquares.ino
+++ FollowYellowGreenSquares/FollowYellowGreenSquares.ino
@@ -34,6 +34,9 @@ void setup()
   driveControl.reset();   //reset NXT motor encoders
   driveControl.stop(MMX_Motor_Both, MMX_Next_Action_Float);
 
+  pinMode(2, INPUT);
+  digitalWrite(2, HIGH);  //enable internal pullup resistor
+
   pixy.init();
   uint16_t w = 0;
   while(w != 0xaa55)
@@ -46,136 +49,206 @@ void setup()
 
 void loop()
 { 
-  static int i = 0;
-  uint16_t blocks;
-
-    // grab blocks!
-  blocks = pixy.getBlocks();
-
-  // Detect and parse blocks
-  if (blocks)
+  // NORMAL MODE
+  if(digitalRead(2) == 0)
   {
-      searchingCounter = 0;
-
-     // found one block
-      if(blocks == 1)
-      {
-        if(pixy.blocks[0].signature == 1)
+    static int i = 0;
+    uint16_t blocks;
+  
+      // grab blocks!
+    blocks = pixy.getBlocks();
+  
+    // Detect and parse blocks
+    if (blocks)
+    {
+        searchingCounter = 0;
+  
+       // found one block
+        if(blocks == 1)
         {
-          state = SINGLE_LOCK_BLUE;
+          if(pixy.blocks[0].signature == 1)
+          {
+            state = SINGLE_LOCK_BLUE;
+          }
+          else if(pixy.blocks[0].signature == 2)
+          {
+            state = SINGLE_LOCK_GREEN;
+          }
+          else
+          {
+            state = ERROR_SPURIOUS;
+          }
+                  
+          //Get distance between x-coordinate of whichever block we see and the center of screen
+          error = pixy.blocks[0].x - PIXY_MAX_X / 2;
         }
-        else if(pixy.blocks[0].signature == 2)
+        
+        // found two blocks
+        else if(blocks == 2)
         {
-          state = SINGLE_LOCK_GREEN;
+          state = DOUBLE_LOCK;
+          int heightDelta = pixy.blocks[1].y - pixy.blocks[0].y;
+          //widthDelta not currently used
+          int widthDelta = pixy.blocks[1].x - pixy.blocks[0].x;
+          if(abs(heightDelta) > 5)
+          {
+            state = ERROR_TILT;
+          }
+          else
+          {
+            // normalizedSeparation not currently used
+            float normalizedSeparation = 2*float(widthDelta)/float(pixy.blocks[0].width + pixy.blocks[1].width);
+            //get distance between the center of the two blocks, and the center of the screen
+            error = (pixy.blocks[1].x + pixy.blocks[0].x - PIXY_MAX_X) / 2;
+          }
         }
+  
+        // found three or more blocks
         else
         {
-          state = ERROR_SPURIOUS;
-        }
-                
-        //Get distance between x-coordinate of whichever block we see and the center of screen
-        error = pixy.blocks[0].x - PIXY_MAX_X / 2;
-      }
-      
-      // found two blocks
-      else if(blocks == 2)
-      {
-        state = DOUBLE_LOCK;
-        int heightDelta = pixy.blocks[1].y - pixy.blocks[0].y;
-        //widthDelta not currently used
-        int widthDelta = pixy.blocks[1].x - pixy.blocks[0].x;
-        if(abs(heightDelta) > 5)
-        {
-          state = ERROR_TILT;
-        }
-        else
-        {
-          // normalizedSeparation not currently used
-          float normalizedSeparation = 2*float(widthDelta)/float(pixy.blocks[0].width + pixy.blocks[1].width);
-          //get distance between the center of the two blocks, and the center of the screen
-          error = (pixy.blocks[1].x + pixy.blocks[0].x - PIXY_MAX_X) / 2;
+          state = ERROR_SPURIOUS;   
         }
       }
-
-      // found three or more blocks
+    
       else
       {
-        state = ERROR_SPURIOUS;   
+        // didn't find any blocks
+        ++searchingCounter;  
       }
-    }
+    
   
-    else
+    if(searchingCounter > 2000)
     {
-      // didn't find any blocks
-      ++searchingCounter;  
+      state = SEARCHING;
+    }
+    
+    //Serial feedback happens periodically
+    if(millis() - timestamp > delay_period )
+    {
+      Serial.write(12); //clear LCD display
+      switch(state)
+        {
+          case ERROR_TILT:
+            Serial.print("Tilted");
+            break;
+          case ERROR_SPURIOUS:
+            Serial.print("Bad Reading");
+            break;
+          case SEARCHING:
+            Serial.print("Searching");
+            break;
+          case SINGLE_LOCK_GREEN:
+            Serial.print("Green Lock");
+            break;
+          case SINGLE_LOCK_BLUE:
+            Serial.print("Blue Lock");
+            break;
+          case DOUBLE_LOCK:
+            Serial.print("Double Lock");
+            break;
+        }
+        Serial.write(13);  // move down a line on LCD
+        Serial.print("E:");
+        Serial.print(error);
+        Serial.print(" L:");
+        Serial.print(int(leftSpeed));
+        Serial.print(" R:");
+        Serial.print(int(rightSpeed));
+        timestamp = millis();
     }
   
-
-  if(searchingCounter > 2000)
-  {
-    state = SEARCHING;
+    switch(state)
+    {
+      case ERROR_TILT:
+        driveControl.stop(MMX_Motor_Both, MMX_Next_Action_Float);
+        break;
+      case ERROR_SPURIOUS:
+        driveControl.stop(MMX_Motor_Both, MMX_Next_Action_Float);
+        break;
+      case SEARCHING:
+        driveControl.runUnlimited(MMX_Motor_1, MMX_Direction_Forward, MMX_Speed_Slow);
+        driveControl.runUnlimited(MMX_Motor_2, MMX_Direction_Reverse, MMX_Speed_Slow);
+        break;
+      case SINGLE_LOCK_BLUE:
+      case SINGLE_LOCK_GREEN:
+      case DOUBLE_LOCK:
+        leftSpeed = baseSpeed + kp * error;
+        rightSpeed = baseSpeed - kp * error;
+        byte motorDirection =  leftSpeed > 0 ? 0x01: 0x00;
+        byte motorSpeed = abs(leftSpeed);
+        driveControl.runUnlimited(MMX_Motor_1, motorDirection, motorSpeed);
+        motorDirection =  rightSpeed > 0 ? 0x01: 0x00;
+        motorSpeed = abs(rightSpeed);
+        driveControl.runUnlimited(MMX_Motor_2, motorDirection, motorSpeed);
+        break;
+    }
   }
   
-  //Serial feedback happens periodically
-  if(millis() - timestamp > delay_period )
+  // CALIBRATION MODE
+  else
   {
-    Serial.write(12); //clear LCD display
-    switch(state)
+    driveControl.stop(MMX_Motor_Both, MMX_Next_Action_Float);
+    Serial.write(12);  //clear
+    Serial.print("speed:");
+
+    int sensorValue = analogRead(A2);
+    int offset = sensorValue - 512;
+    int range = map(offset, 512, -512, 5, 0)-1;
+    if(range != 2)
+    {
+      switch(range) 
       {
-        case ERROR_TILT:
-          Serial.print("Tilted");
+        case 0:
+          baseSpeed -= 0.5;
           break;
-        case ERROR_SPURIOUS:
-          Serial.print("Bad Reading");
+         case 1:
+          baseSpeed -= 0.1;
           break;
-        case SEARCHING:
-          Serial.print("Searching");
+         case 3:
+          baseSpeed += 0.1;
           break;
-        case SINGLE_LOCK_GREEN:
-          Serial.print("Green Lock");
-          break;
-        case SINGLE_LOCK_BLUE:
-          Serial.print("Blue Lock");
-          break;
-        case DOUBLE_LOCK:
-          Serial.print("Double Lock");
+         case 4:
+          baseSpeed += 0.5;
           break;
       }
-      Serial.write(13);  // move down a line on LCD
-      Serial.print("E:");
-      Serial.print(error);
-      Serial.print(" L:");
-      Serial.print(int(leftSpeed));
-      Serial.print(" R:");
-      Serial.print(int(rightSpeed));
-      timestamp = millis();
+      constrain_(baseSpeed, 0.0, 100.0);
+    }
+    Serial.print(baseSpeed);
+    Serial.write(13);  //move down
+    Serial.print("kp:");
+    
+    sensorValue = analogRead(A3);
+    offset = sensorValue - 512;
+    range = map(offset, -512, 512, 5, 0)-1;
+    if(range != 2)
+    {
+      switch(range) 
+      {
+        case 0:
+          kp -= 0.1;
+          break;
+         case 1:
+          kp -= 0.01;
+          break;
+         case 3:
+          kp += 0.01;
+          break;
+         case 4:
+          kp += 0.1;
+          break;
+      }
+      constrain_(kp, 0.0, 5.0);
+    }
+    Serial.print(kp);
+    delay(250); 
   }
+}
 
-  switch(state)
-  {
-    case ERROR_TILT:
-      driveControl.stop(MMX_Motor_Both, MMX_Next_Action_Float);
-      break;
-    case ERROR_SPURIOUS:
-      driveControl.stop(MMX_Motor_Both, MMX_Next_Action_Float);
-      break;
-    case SEARCHING:
-      driveControl.stop(MMX_Motor_Both, MMX_Next_Action_Float);
-      break;
-    case SINGLE_LOCK_BLUE:
-    case SINGLE_LOCK_GREEN:
-    case DOUBLE_LOCK:
-      leftSpeed = baseSpeed + kp * error;
-      rightSpeed = baseSpeed - kp * error;
-      byte motorDirection =  leftSpeed > 0 ? 0x01: 0x00;
-      byte motorSpeed = abs(leftSpeed);
-      driveControl.runUnlimited(MMX_Motor_1, motorDirection, motorSpeed);
-      motorDirection =  rightSpeed > 0 ? 0x01: 0x00;
-      motorSpeed = abs(rightSpeed);
-      driveControl.runUnlimited(MMX_Motor_2, motorDirection, motorSpeed);
-      break;
-  }
-
+float constrain_(float value, float minValue, float maxValue)
+{
+  if(value > minValue)  value = minValue;
+  else if(value > maxValue) value = maxValue;
+  return value;
 }
 
 
